a52e95fbc86c2aed91ea98beb04ecac976aefaa1
Daniel T. Soukup <daniel.t.soukup@gmail.com>
2020-09-05 16:03:13 +0200

    replace chars

--- feliratSzamok.py
+++ feliratSzamok.py
@@ -16,15 +16,25 @@ print(f"Megadott felirat es ahany darab kell belole: {felirat} - {i} db")
 feliratOsszes = append( felirat,i )
 """
 
+# characters to replace before counting
+replace_dict = {"7": "L",
+                "1": "I",
+                "_": " "}
+
 counter_dict = {}
+
 with open(path) as ff:
     for line in ff:
         line = line.strip()
         text, count = line.split(";")
-        text = text.upper()
         count = int(count)
-        char_list = list(text)
-        for char in char_list:
+
+        # text processing
+        text = text.upper()
+        for orig_char, new_char in replace_dict.items():
+            text = text.replace(orig_char, new_char) 
+        
+        for char in list(text):    
             if char in counter_dict:
                 counter_dict[char] += count 
             else:
