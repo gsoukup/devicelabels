dcfc916ead288894fb5e3a363ee3a1d85269cdae
Gabe Soukup <gabor.soukup@gmail.com>
2020-09-05 15:32:55 +0200

    Basic files

---
+++ feliratSzamok.py
@@ -0,0 +1,28 @@
+# ./usr/bin/env python
+# -*- Coding:utf-8 -*-
+# Mikros feliratokhoz szukseges szamok darab-darab
+
+#Deklaracio
+adatok = []
+path = "betuk.txt"
+
+#Main
+print("Add meg a feliratokat es hogy hany darab szukseges!")
+
+"""
+i = input("Felirat darab szama:")
+felirat = input("Felirat:")
+print(f"Megadott felirat es ahany darab kell belole: {felirat} - {i} db")
+feliratOsszes = append( felirat,i )
+"""
+
+with open(path) as ff:
+
+    for line in ff:
+
+        line = line.strip()
+        line = line.split(";")
+        adatok.append(line)
+        print(adatok)
+
+
